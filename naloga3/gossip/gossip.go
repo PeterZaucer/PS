e236733277fef6698ebc678a3c637bf5876a5790
Paucerus <pz77679@student.uni-lj.si>
2024-12-17 22:01:37 +0100

    run_file

--- naloga3/gossip/gossip.go
+++ naloga3/gossip/gossip.go
@@ -9,19 +9,15 @@ import (
 	"github.com/DistributedClocks/GoVector/govec"
 )
 
-// Message structure for gossiping
 type Message struct {
 	ID   int
 	Data string
 }
 
-// Start initializes the gossip protocol
 func Start(id, N, M, K, basePort int) {
-	// Initialize logger
 	logger := govec.InitGoVector(fmt.Sprintf("Process-%d", id), fmt.Sprintf("Log-%d", id), govec.GetDefaultConfig())
 	opts := govec.GetDefaultLogOptions()
 
-	// Set up UDP listening
 	localAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("localhost:%d", basePort+id))
 	checkError(err)
 	conn, err := net.ListenUDP("udp", localAddr)
@@ -29,9 +25,8 @@ func Start(id, N, M, K, basePort int) {
 	defer conn.Close()
 
 
-	// Special behavior for the master process
 	if id == 0 {
-		time.Sleep(1 * time.Second) // Ensure all processes are ready before dissemination
+		time.Sleep(1 * time.Second)
 		go listenMaster(conn, id, logger, opts, M)
 		for i := 1; i <= M; i++ {
 			msg := Message{ID: i, Data: fmt.Sprintf("Message-%d", i)}
@@ -48,9 +43,8 @@ func Start(id, N, M, K, basePort int) {
 
 func listenMaster(conn *net.UDPConn,  id int, logger *govec.GoLog, opts govec.GoLogOptions, M int) {
 	buffer := make([]byte, 1024)
-	received := map[int]bool{} // Master already "knows" the messages it sends
+	received := map[int]bool{}
 
-	// Initialize the master's received map with all messages it will send
 	for i := 1; i <= M; i++ {
 		received[i] = true
 	}
@@ -60,7 +54,6 @@ func listenMaster(conn *net.UDPConn,  id int, logger *govec.GoLog, opts govec.Go
 		_, _, err := conn.ReadFromUDP(buffer)
 
 		if err, ok := err.(net.Error); ok && err.Timeout() {
-			// Terminate if no message is received before the timeout
 			fmt.Printf("Process %d: No messages received. Terminating.\n", id)
 			return
 		} else if err != nil {
@@ -68,23 +61,20 @@ func listenMaster(conn *net.UDPConn,  id int, logger *govec.GoLog, opts govec.Go
 			continue
 		}
 
-		// Unpack message and vector clock
 		var msg Message
 		logger.UnpackReceive("Received Message", buffer, &msg, opts)
 	}
 }
 
-// listen handles incoming messages
 func listen(conn *net.UDPConn, id, N, K int, logger *govec.GoLog, opts govec.GoLogOptions) {
 	buffer := make([]byte, 1024)
-	received := map[int]bool{} // Track received messages
+	received := map[int]bool{}
 
 	for {
 		conn.SetReadDeadline(time.Now().Add(10 * time.Second))
 		_, _, err := conn.ReadFromUDP(buffer)
 
 		if err, ok := err.(net.Error); ok && err.Timeout() {
-			// Terminate if no message is received before the timeout
 			fmt.Printf("Process %d: No messages received. Terminating.\n", id)
 			return
 		} else if err != nil {
@@ -92,24 +82,19 @@ func listen(conn *net.UDPConn, id, N, K int, logger *govec.GoLog, opts govec.GoL
 			continue
 		}
 
-		// Unpack message and vector clock
 		var msg Message
 		logger.UnpackReceive("Received Message", buffer, &msg, opts)
 
-		// Print only new messages
 		if !received[msg.ID] {
 			fmt.Printf("Process %d received: %s\n", id, msg.Data)
 			received[msg.ID] = true
 
-			// Forward message to K random processes
 			sendMessage(id, N, K, msg, conn.LocalAddr().(*net.UDPAddr).Port, logger, opts)
 		}
 	}
 }
 
-// sendMessage forwards a message to K random processes
 func sendMessage(id, N, K int, msg Message, basePort int, logger *govec.GoLog, opts govec.GoLogOptions) {
-	// Select K random processes to send the message
 	targets := selectRandomTargets(id, N, K)
 
 	for _, target := range targets {
@@ -118,7 +103,6 @@ func sendMessage(id, N, K int, msg Message, basePort int, logger *govec.GoLog, o
 		conn, err := net.DialUDP("udp", nil, addr)
 		checkError(err)
 
-		// Prepare and send message
 		logger.LogLocalEvent("Preparing Message", opts)
 		msgVC := logger.PrepareSend("Sending Message", msg, opts)
 		_, err = conn.Write(msgVC)
@@ -127,7 +111,6 @@ func sendMessage(id, N, K int, msg Message, basePort int, logger *govec.GoLog, o
 	}
 }
 
-// selectRandomTargets picks K unique random targets
 func selectRandomTargets(id, N, K int) []int {
 	rand.Seed(time.Now().UnixNano())
 	targets := map[int]bool{}
@@ -144,7 +127,6 @@ func selectRandomTargets(id, N, K int) []int {
 	return keys
 }
 
-// checkError handles errors
 func checkError(err error) {
 	if err != nil {
 		panic(err)
